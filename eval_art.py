0ab2b497f697e5f329f8dd57911afcdc2dd91621
Wenqin SHAO <samuelshwq@gmail.com>
2017-02-11 19:42:15 +0100

    benchmark changedetection with artifical traces

--- eval_art.py
+++ eval_art.py
@@ -6,6 +6,41 @@ import os
 from localutils import changedetect as dc, benchmark as bch
 import logging
 import ConfigParser
+import traceback
+import multiprocessing
+
+METHOD = ['cpt_normal', 'cpt_poisson', 'cpt_np']
+PENALTY = ["SIC", "BIC", "MBIC", "AIC", "Hannan-Quinn"]
+WINDOW = 2  # perform evaluation with window size equaling 2
+
+
+def worker(f):
+    f_base = os.path.basename(f)
+    r = []
+    logging.info("handling %s" % f)
+    trace = pd.read_csv(f, sep=';')
+    fact = trace['cp']
+    fact = [i for i, v in enumerate(fact) if v == 1]  # fact in format of data index
+    logging.debug("%s : change counts %d" % (f_base, len(fact)))
+    for m, p in [(x, y) for x in METHOD for y in PENALTY]:
+        logging.debug("%s: evaluating %s with %s" % (f_base, m, p))
+        method_caller = getattr(dc, m)
+        detect = method_caller(trace['rtt'], penalty=p)
+        b = bch.evaluation_window_weighted(trace['rtt'], fact, detect, WINDOW)
+        r.append((os.path.basename(f), len(trace), len(fact),
+                  b['tp'], b['fp'], b['fn'],
+                  b['precision'], b['recall'], b['score'], b['dis'], m+'&'+p))
+        logging.debug('%r' % b)
+    return r
+
+
+def worker_wrapper(args):
+    try:
+        return worker(args)
+    except Exception:
+        logging.critical("Exception in worker.")
+        traceback.print_exc()
+        raise
 
 
 def main():
@@ -44,34 +79,22 @@ def main():
         logging.critical("folder %s does not exisit." % data_dir)
         return
 
-    res = dict()
-    window = 2  # perform evaluation with window size equaling 2
+    files = []
     for f in os.listdir(art_dir):
         if f.endswith('.csv') and not f.startswith('~'):
-            logging.info("handling %s" % f)
-            trace = pd.read_csv(os.path.join(art_dir, f), sep=';')
-            fact = trace['cp']
-            fact = [i for i, v in enumerate(fact) if v == 1]  # fact in format of data index
-            pred_normal = dc.cpt_normal(trace['rtt'])
-            pred_np = dc.cpt_np(trace['rtt'])
-            res[f] = dict(len=len(fact), changes=len(fact),
-                          normal=bch.evaluation_window_weighted(trace['rtt'],
-                                                                fact,
-                                                                pred_normal, window),
-                          np=bch.evaluation_window_weighted(trace['rtt'],
-                                                            fact,
-                                                            pred_np, window))
+            files.append(os.path.join(art_dir,f))
+
+    pool = multiprocessing.Pool(processes=2)
+    res = pool.map(worker_wrapper, files)
+
+    #res = [worker(i) for i in files]
 
     with open(os.path.join(data_dir, 'eval_art.csv'), 'w') as fp:
         fp.write(';'.join(
-            ['file', 'len', 'changes', 'tp', 'fp', 'fn', 'precision', 'recall', 'dis', 'score', 'method']) + '\n')
-        for f in res:
-            for m in ['normal', 'np']:
-                line = ';'.join([str(i) for i in [f, res[f]['len'], res[f]['changes'],
-                                                  res[f][m]['tp'], res[f][m]['fp'], res[f][m]['fn'],
-                                                  res[f][m]['precision'], res[f][m]['recall'],
-                                                  res[f][m]['dis'], res[f][m]['score'], m]]) + '\n'
-                fp.write(line)
+            ['file', 'len', 'changes', 'tp', 'fp', 'fn', 'precision', 'recall', 'score', 'dis', 'method']) + '\n')
+        for ck in res:
+            for line in ck:
+                fp.write(";".join([str(i) for i in line]) + '\n')
 
 
 if __name__ == '__main__':
