95d616999b31ec6d82dce8c33e8d139178441f79
Wenqin SHAO <samuelshwq@gmail.com>
2017-02-12 18:17:44 +0100

    handle case for poisson where all values are negative

--- localutils/changedetect.py
+++ localutils/changedetect.py
@@ -2,6 +2,8 @@
 changedetect.py provides tools for detecting changes in RTT time series
 """
 import numpy as np
+import logging
+from rpy2.rinterface import RRuntimeError
 from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import IntVector, FloatVector
 changepoint = importr('changepoint')
@@ -23,6 +25,7 @@ def cpt_normal(x, penalty="MBIC", minseglen=1):
     return [int(i) for i in changepoint.cpts(changepoint.cpt_meanvar(FloatVector(x),
                                                                      test_stat='Normal', method='PELT',
                                                                      penalty=penalty, minseglen=minseglen))]
+
 
 
 def cpt_np(x, penalty="MBIC", minseglen=1):
@@ -56,7 +59,10 @@ def cpt_poisson(x, penalty="MBIC", minseglen=1):
             since the R indexing starts from 1, the return naturally become the beginning of segment.
         """
     x = np.rint(x)
-    base = np.min([i for i in x if i > 0])
+    try:
+        base = np.min([i for i in x if i > 0])
+    except ValueError:  # if no positive number if x, set base to 0
+        base = 0
     x = [i-base if i > 0 else 1e3 for i in x]
     return [int(i) for i in changepoint.cpts(changepoint.cpt_meanvar(IntVector(x), test_stat='Poisson',
                                                                      method='PELT', penalty=penalty,
